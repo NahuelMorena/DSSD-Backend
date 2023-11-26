fba309e76f28c8953c259366f85a144bac73aa42
Nahuel Morena <nahuel1morena1@gmail.com>
2023-11-25 22:10:02 -0300

    bugfix send mails

--- src/main/java/dssd/global/furniture/backend/controllers/MailController.java
+++ src/main/java/dssd/global/furniture/backend/controllers/MailController.java
@@ -5,16 +5,19 @@ import dssd.global.furniture.backend.services.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MailController {
     private final String baseUrl = "/api/mail";
     @Autowired
     private MailService mailService;
-    @PostMapping(baseUrl + "/sendMailToIndicateStartOfManufacturing/{mail}")
+    @CrossOrigin(origins = "http://localhost:60000", allowCredentials = "true")
+    @GetMapping(baseUrl + "/sendMailToIndicateStartOfManufacturing/{mail}")
     public ResponseEntity<String> sendMailToIndicateStartOfManufacturing(@PathVariable String mail){
+        System.out.println("Se lanzara mail de inicio");
         MailStructure mailStructure = new MailStructure(
                 "Indicativo de iniciación de la fabricación",
                 "Se informa que la tarea de fabricación de la colección ha iniciado. Se informara cuando este proceso finalice."
@@ -22,9 +25,10 @@ public class MailController {
         mailService.sendMail(mail,mailStructure);
         return ResponseEntity.ok("El mail fue enviado con exito");
     }
-
-    @PostMapping(baseUrl + "/sendMailToIndicateEndOfManufacturing/{mail}")
+    @CrossOrigin(origins = "http://localhost:60000", allowCredentials = "true")
+    @GetMapping(baseUrl + "/sendMailToIndicateEndOfManufacturing/{mail}")
     public ResponseEntity<String> sendMailToIndicateEndOfManufacturing(@PathVariable String mail){
+        System.out.println("Se lanzara mail de fin");
         MailStructure mailStructure = new MailStructure(
                 "Indicativo de finalización en la fabricación",
                 "Se informa que la tarea de fabricación de la colección ha sido completada satisfactoriamente"
